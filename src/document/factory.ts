f64aeea553902b0f805721722e1b9afdc77254cd
Duc Trung Le <leductrungxf@gmail.com>
2025-05-31 18:51:38 +0200

    remove unused class

--- src/document/factory.ts
+++ src/document/factory.ts
@@ -1,9 +1,10 @@
 import { ABCWidgetFactory, DocumentRegistry } from '@jupyterlab/docregistry';
 
-import { NotebookSpectaPanel, NotebookSpectaDocWidget } from './widget';
+import { NotebookSpectaDocWidget } from './widget';
 
 import { INotebookModel } from '@jupyterlab/notebook';
 import { SpectaWidgetFactory } from '../specta_widget_factory';
+import { Widget } from '@lumino/widgets';
 
 interface IOptions extends DocumentRegistry.IWidgetFactoryOptions {
   spectaWidgetFactory: SpectaWidgetFactory;
@@ -21,11 +22,21 @@ export class NotebookGridWidgetFactory extends ABCWidgetFactory<
   protected createNewWidget(
     context: DocumentRegistry.IContext<INotebookModel>
   ): NotebookSpectaDocWidget {
-    // const content = new Widget();
+    const content = new Widget();
+    content.addClass('jp-specta-notebook-panel');
+    context.ready.then(async () => {
+      const spectaWidget = await this._spectaWidgetFactory.createNew({
+        context
+      });
+
+      if (spectaWidget) {
+        Widget.attach(spectaWidget, content.node);
+      }
+    });
 
     const widget = new NotebookSpectaDocWidget({
       context,
-      content: new NotebookSpectaPanel(context, this._spectaWidgetFactory)
+      content
     });
 
     return widget;
--- src/document/widget.ts
+++ src/document/widget.ts
@@ -1,5 +1,5 @@
 import { IEditorMimeTypeService } from '@jupyterlab/codeeditor';
-import { DocumentRegistry, DocumentWidget } from '@jupyterlab/docregistry';
+import { DocumentWidget } from '@jupyterlab/docregistry';
 import {
   INotebookModel,
   INotebookTracker,
@@ -7,11 +7,7 @@ import {
 } from '@jupyterlab/notebook';
 import { IRenderMimeRegistry } from '@jupyterlab/rendermime';
 import { ServiceManager } from '@jupyterlab/services';
-import { ISessionContext } from '@jupyterlab/apputils';
-import { Signal } from '@lumino/signaling';
 import { Widget } from '@lumino/widgets';
-import { SpectaWidgetFactory } from '../specta_widget_factory';
-import { AppWidget } from '../specta_widget';
 
 export interface ISpectaOptions {
   manager: ServiceManager;
@@ -22,15 +18,10 @@ export interface ISpectaOptions {
 }
 
 export class NotebookSpectaDocWidget extends DocumentWidget<
-  NotebookSpectaPanel,
+  Widget,
   INotebookModel
 > {
-  constructor(
-    private options: DocumentWidget.IOptions<
-      NotebookSpectaPanel,
-      INotebookModel
-    >
-  ) {
+  constructor(options: DocumentWidget.IOptions<Widget, INotebookModel>) {
     super(options);
   }
 
@@ -38,47 +29,4 @@ export class NotebookSpectaDocWidget extends DocumentWidget<
     this.content.dispose();
     super.dispose();
   }
-
-  get sessionContext(): ISessionContext | undefined {
-    return this.options.content.sessionContext;
-  }
-}
-
-export class NotebookSpectaPanel extends Widget {
-  constructor(
-    context: DocumentRegistry.IContext<INotebookModel>,
-    spectaWidgetFactory: SpectaWidgetFactory
-  ) {
-    super();
-    this.addClass('jp-specta-notebook-panel');
-    context.ready.then(async () => {
-      const spectaWidget = await spectaWidgetFactory.createNew({
-        context
-      });
-
-      if (spectaWidget) {
-        this._widget = spectaWidget;
-        Widget.attach(this._widget, this.node);
-      }
-    });
-  }
-
-  dispose(): void {
-    if (this.isDisposed) {
-      return;
-    }
-    this._widget?.dispose();
-    Signal.clearData(this);
-    super.dispose();
-  }
-
-  get sessionContext(): ISessionContext | undefined {
-    return this._widget?.model.context?.sessionContext;
-  }
-
-  get model(): INotebookModel | undefined {
-    return this._widget?.model.context?.model;
-  }
-
-  private _widget: AppWidget | undefined;
 }
