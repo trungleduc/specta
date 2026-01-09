572979fc3e0e0ffa6c379291f8fe473d80cc461a
Duc Trung Le <leductrungxf@gmail.com>
2026-01-09 21:16:40 +0100

    fix missing file editor service (#38)
    * fix missing file editor service
    
    * lint

--- src/sharedscope.ts
+++ src/sharedscope.ts
@@ -1,4 +1,5 @@
-import '@jupyterlab/logconsole';
 import '@jupyterlab/console';
+import '@jupyterlab/fileeditor';
+import '@jupyterlab/logconsole';
 import '@jupyterlite/apputils';
 import 'mock-socket';
